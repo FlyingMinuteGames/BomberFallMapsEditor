bafad082a7d3892b24f1bb57fcc7b02cd8524483
Agamand <Basset.cyril@gmail.com>
2013-10-17 02:59:13 +0200

    Update editor v0.0.1, add brush system, save and load

---
+++ Assets/GUI/EditorMenu.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EditorMenu : MonoBehaviour {
+    
+	// Use this for initialization
+
+    private string grid_x="5", grid_y="5";
+    private int x, y;
+    private Maps maps = null;
+    public Material grid_mat;
+    public Material ground_mat;
+    public Material cursor_mat;
+    public Transform stone;
+    public Transform breakable;
+    public Transform cursor_tpl;
+    public static float maxZoom = 6.0f;
+   
+
+
+    Plane plane = new Plane(new Vector3(0, 1, 0), new Vector3(0, 0, 0));
+    GameObject cursor;
+    int selGridInt;
+    int parseInt(string input)
+    {
+        int size;
+        // best than int.Parse() !
+        if(input == null || (size = input.Length) == 0)
+            return 0;
+        int i = 0,value = 0,s=1;
+        while (char.IsWhiteSpace(input[i])) i++; // escape space !
+        if(input[i] == '-')
+        {
+            s = -s;
+        }
+        while(i < size && input[i] != 0)
+        {
+            if(input[i] > '9' || input[i] < '0')
+                break;
+            value = value * 10 + input[i] - '0';
+            i++;
+            
+        }
+        return (s*value);
+    }
+	void Start () {
+
+        //tmp hack
+        Maps.s_material = ground_mat;
+        Maps.s_grid = grid_mat;
+        Maps.s_stone = stone;
+        Maps.s_breakable = breakable;
+        cursor = ((Transform)Instantiate(cursor_tpl)).gameObject;
+        /*cursor = new GameObject(name);
+        Mesh plane = cursor.AddComponent<MeshFilter>().mesh;
+        cursor.AddComponent<MeshRenderer>();
+        plane.vertices = Maps._Vertices;
+        plane.uv = Maps._UV;
+        plane.triangles = Maps._Indices;
+        plane.normals = Maps._Normal;
+        cursor.renderer.material = cursor_mat;*/
+        cursor.transform.position.Set(0,0.52f,0);
+        //cursor.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float wheel = 0;
+        if ((wheel = Input.GetAxis("Mouse ScrollWheel")) < 0)
+        {
+            Camera.main.transform.position += new Vector3(0, -wheel * 2, 0);
+        }
+        else if ((wheel = Input.GetAxis("Mouse ScrollWheel")) > 0)
+        {
+
+            Camera.main.transform.position += new Vector3(0, -wheel * 2, 0);
+            if (Camera.main.transform.position.y < maxZoom)
+                Camera.main.transform.position += new Vector3(0, 6 - Camera.main.transform.position.y, 0);
+        }
+        float hit;
+
+        Ray r;
+        if (maps != null)
+            if (plane.Raycast(r = Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                
+                Vector3 pos = r.GetPoint(hit);
+                Vector3 o = new Vector3(pos.x, pos.y, pos.z);
+                pos.y = 0.52f;
+                bool test;
+                pos = maps.NormalizePosition(pos,out test);
+                if(!test)
+                    return;
+                cursor.transform.position = pos;
+                //Debug.Log(cursor.transform.position + " " + o);
+
+                if (Input.GetMouseButton(0))
+                {
+                    
+                    IntVector2 tpos = maps.GetTilePosition(new Vector2(pos.x, pos.z));
+                    if (tpos != null)
+                        maps.AddBlock((MapsTiles)selGridInt, tpos);
+                    
+                }
+                else if (Input.GetMouseButton(1))
+                {
+                    IntVector2 tpos = maps.GetTilePosition(new Vector2(pos.x, pos.z));
+                    if (tpos != null)
+                        maps.AddBlock((MapsTiles)0, tpos);
+                    
+                }
+
+            }
+	}
+
+    void addblock(IntVector2 curtile)
+    { 
+
+    }
+    void OnGUI()
+    {
+        
+        GUI.Box(GuiUtils.ResizeGUI(new Rect(10, 50, 150, 500),true), "");
+        GUI.Label(GuiUtils.ResizeGUI(new Rect(15, 60, 80, 30), true), "Grid size :");
+        grid_x = (x = parseInt(GUI.TextField(GuiUtils.ResizeGUI(new Rect(15, 85, 45, 25), true), grid_x))).ToString();
+        grid_y = (y = parseInt(GUI.TextField(GuiUtils.ResizeGUI(new Rect(62, 85, 45, 25), true), grid_y))).ToString();
+        selGridInt = GUI.SelectionGrid(GuiUtils.ResizeGUI(new Rect(30, 150, 100, 100), true), selGridInt, new string[] { "Empty tiles", "Solid block", "Breakable block" }, 1);
+        if (GUI.Button(GuiUtils.ResizeGUI(new Rect(15, 120, 140, 25), true), "Generate maps"))
+        {
+
+            if (maps == null)
+                maps = new Maps(new IntVector2(x, y));
+            else
+            {
+                maps.Clear();
+                maps.Size = new IntVector2(x, y);
+            }
+            maps.Generate();
+        
+        }
+        if (GUI.Button(GuiUtils.ResizeGUI(new Rect(15, 260, 140, 25), true), "Fill"))
+        {
+
+            maps.Fill((MapsTiles)selGridInt);
+
+        }
+
+        if (GUI.Button(GuiUtils.ResizeGUI(new Rect(15, 290, 140, 25), true), "Save to"))
+        {
+            if(maps != null)
+                maps.SaveToFile("test_map.map");
+        }
+
+        if (GUI.Button(GuiUtils.ResizeGUI(new Rect(15, 320, 140, 25), true), "Load from"))
+        {
+            Debug.Log("test");
+            maps = Maps.LoadMapsFromFile("test_map.map");
+            //maps.Clear();
+            //maps.Generate();
+        }
+        
+
+    }
+}
